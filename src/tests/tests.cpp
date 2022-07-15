a9436e01ca73dbb8b4017b737c0c3b9fcf1ecfe4
emufan4568 <geoster3d@gmail.com>
2022-07-15 16:37:32 +0300

    tests: Update test suite for Catch2 3.0.1

--- src/tests/tests.cpp
+++ src/tests/tests.cpp
@@ -1,9 +1,10 @@
-// Copyright 2016 Citra Emulator Project
+// Copyright 2022 Citra Emulator Project
 // Licensed under GPLv2 or any later version
 // Refer to the license.txt file included.
 
-#define CATCH_CONFIG_MAIN
-#include <catch2/catch.hpp>
+#include <catch2/catch_session.hpp>
 
-// Catch provides the main function since we've given it the
-// CATCH_CONFIG_MAIN preprocessor directive.
+// Standard C/C++ main entry point
+int main(int argc, char* argv[]) {
+    return Catch::Session().run(argc, argv);
+}
--- src/tests/video_core/shader/shader_jit_x64_compiler.cpp
+++ src/tests/video_core/shader/shader_jit_x64_compiler.cpp
@@ -5,7 +5,8 @@
 #include <algorithm>
 #include <cmath>
 #include <memory>
-#include <catch2/catch.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/catch_approx.hpp>
 #include <nihstro/inline_assembly.h>
 #include "video_core/shader/shader_jit_x64_compiler.h"
 
@@ -65,9 +66,9 @@ TEST_CASE("LG2", "[video_core][shader][shader_jit]") {
     REQUIRE(std::isnan(shader.Run(NAN)));
     REQUIRE(std::isnan(shader.Run(-1.f)));
     REQUIRE(std::isinf(shader.Run(0.f)));
-    REQUIRE(shader.Run(4.f) == Approx(2.f));
-    REQUIRE(shader.Run(64.f) == Approx(6.f));
-    REQUIRE(shader.Run(1.e24f) == Approx(79.7262742773f));
+    REQUIRE(shader.Run(4.f) == Catch2::Approx(2.f));
+    REQUIRE(shader.Run(64.f) == Catch2::Approx(6.f));
+    REQUIRE(shader.Run(1.e24f) == Catch2::Approx(79.7262742773f));
 }
 
 TEST_CASE("EX2", "[video_core][shader][shader_jit]") {
@@ -82,10 +83,10 @@ TEST_CASE("EX2", "[video_core][shader][shader_jit]") {
     });
 
     REQUIRE(std::isnan(shader.Run(NAN)));
-    REQUIRE(shader.Run(-800.f) == Approx(0.f));
-    REQUIRE(shader.Run(0.f) == Approx(1.f));
-    REQUIRE(shader.Run(2.f) == Approx(4.f));
-    REQUIRE(shader.Run(6.f) == Approx(64.f));
-    REQUIRE(shader.Run(79.7262742773f) == Approx(1.e24f));
+    REQUIRE(shader.Run(-800.f) == Catch2::Approx(0.f));
+    REQUIRE(shader.Run(0.f) == Catch2::Approx(1.f));
+    REQUIRE(shader.Run(2.f) == Catch2::Approx(4.f));
+    REQUIRE(shader.Run(6.f) == Catch2::Approx(64.f));
+    REQUIRE(shader.Run(79.7262742773f) == Catch2::Approx(1.e24f));
     REQUIRE(std::isinf(shader.Run(800.f)));
 }
